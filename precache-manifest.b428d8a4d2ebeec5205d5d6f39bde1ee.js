32955136de7faa688c7eb8450c274d05625ea2ea
Faiz Ahmed <faaezahmd@gmail.com>
2020-05-03 02:16:28 +0500

    First Commit

---
+++ precache-manifest.b428d8a4d2ebeec5205d5d6f39bde1ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fa6f5214c7451977bf1b602234789c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44f344291335fcf31e15",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4da16c3d9889fd079117",
+    "url": "/static/css/main.ad1b62db.chunk.css"
+  },
+  {
+    "revision": "44f344291335fcf31e15",
+    "url": "/static/js/2.c7605478.chunk.js"
+  },
+  {
+    "revision": "dee0a73e24ab96006a54de89755fc2f6",
+    "url": "/static/js/2.c7605478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4da16c3d9889fd079117",
+    "url": "/static/js/main.6debd093.chunk.js"
+  },
+  {
+    "revision": "e03bcb32be44fcf0e726",
+    "url": "/static/js/runtime-main.5f1edeeb.js"
+  }
+]);
